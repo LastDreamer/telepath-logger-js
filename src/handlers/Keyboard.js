66c1b91ac072d68752af97d51e835ada45085f7b
LastDreamer <oleg.kravchenko88@gmail.com>
2017-07-10 02:11:46 +0500

    Добавил немного метрик

--- src/handlers/Keyboard.js
+++ src/handlers/Keyboard.js
@@ -5,6 +5,64 @@ Keyboard.init = (callback) => {
   Keyboard.start();
 
   Keyboard.on('press', data => {
-    callback(data.data);
+    let key = data.data;
+    switch (key) {
+    case '<Space>':
+      key = '␣';
+      break;
+    case '<LCommand>':
+    case '<RCommand>':
+      key = '↓⌘';
+      break;
+    case '[released <LCommand>]':
+    case '[released <RCommand>]':
+      key = '↑⌘';
+      break;
+    case '<Up>':
+      key = '↑';
+      break;
+    case '<Down>':
+      key = '↓';
+      break;
+    case '<Left>':
+      key = '←';
+      break;
+    case '<Right>':
+      key = '→';
+      break;
+    case '<LShift>':
+    case '<RShift>':
+      key = '⇧';
+      break;
+    case '<Enter>':
+      key = '↵';
+      break;
+    case '<LOption>':
+    case '<ROption>':
+      key = '⌥';
+      break;
+    case '<LCtrl>':
+    case '<RCtrl>':
+      key = '^';
+      break;
+    case '<ESC>':
+      key = '⎋';
+      break;
+    case '<Delete>':
+      key = '⌫';
+      break;
+    case '<Tab>':
+      key = '⇥';
+      break;
+    case '[released <LShift>]':
+    case '[released <RShift>]':
+    case '[released <LOption>]':
+    case '[released <ROption>]':
+    case '[released <LCtrl>]':
+    case '[released <RCtrl>]':
+      key = '';
+      break;
+    }
+    callback(key);
   });
 };
--- src/index.js
+++ src/index.js
@@ -8,12 +8,21 @@ const app = express();
 const server = http.Server(app);
 const io = socket(server);
 
+let currentApp = '';
+let keystrokes = 0;
+let windowSwitches = 0;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.emit('changedApp', currentApp);
+  socket.emit('changedApp', currentApp);
+  socket.emit('shitchWindow', windowSwitches);
 });
 
 server.listen(3000, function(){
@@ -25,12 +34,30 @@ server.listen(3000, function(){
 //Logging handlers
 Keyboard.init((key) => {
   activeWin().then(result => {
-    console.log(result.app, key);
+    if (result.app != currentApp) {
+      console.log(result.app);
+      currentApp = result.app;
+      io.emit('changedApp', result.app);
+      windowSwitches++;
+      io.emit('shitchWindow', windowSwitches);
+    }
   });
+
+  if (key) {
+    keystrokes++;
+    io.emit('changedKeystrokes', keystrokes);
+    io.emit('lastChar', key);
+  }
 });
 
 Mouse.init((event) => {
   activeWin().then(result => {
-    console.log(result.app, event.type);
+    if (result.app != currentApp) {
+      currentApp = result.app;
+      io.emit('changedApp', result.app);
+      console.log(result.title);
+      windowSwitches++;
+      io.emit('shitchWindow', windowSwitches);
+    }
   });
 });
